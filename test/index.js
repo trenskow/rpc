8fb4d98b52eba72a16b899a1fbeb6379510abead
Kristian Trenskow <this.is@kristians.email>
2025-12-21 14:11:36 +0100

    Added a test.

---
+++ test/index.js
@@ -0,0 +1,76 @@
+//
+// index.js
+// 0-bit-games-frontend-vscode
+//
+// Created by Kristian Trenskow on 2025/12/21
+// See license in LICENSE.
+//
+
+import { EventEmitter } from 'events';
+
+import { use as chaiUse, expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import rpc from '../lib/index.js';
+
+class Transport extends EventEmitter {
+
+	constructor() {
+		super();
+		this._transports = [];
+	}
+
+	addTransport(transport) {
+		this._transports.push(transport);
+	}
+
+	send(command, data) {
+		for (const transport of this._transports) {
+			transport.emit('message', {
+				command,
+				data
+			});
+		}
+	}
+
+};
+
+chaiUse(chaiAsPromised);
+
+const serverTransport = new Transport();
+const clientTransport = new Transport();
+
+serverTransport.addTransport(clientTransport);
+clientTransport.addTransport(serverTransport);
+
+const server = rpc.serve({
+	async add(a, b) {
+		return a + b;
+	}
+}, (command, data) => {
+	serverTransport.send(command, data);
+});
+
+const client = rpc.connect((command, data) => {
+	clientTransport.send(command, data);
+});
+
+serverTransport.on('message', ({ command, data }) => {
+	server.onMessage(command, ...data);
+});
+
+clientTransport.on('message', ({ command, data }) => {
+	client.onMessage(command, data);
+});
+
+describe('RPC', () => {
+
+	describe('Basic Functionality', () => {
+
+		it('should perform a basic RPC call', () => {
+			return expect(client.remote.add(2, 3)).to.eventually.equal(5);
+		});
+
+	});
+
+});
